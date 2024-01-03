c5a8db8ed4f0fb077349dd7302d3e4d8875103ec
Pooja <poojashres7@gmail.com>
2024-01-03 07:33:40 +0545

    treeleaf task 1

---
+++ treeleaf/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Task one
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/pooja-shrestha/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pooja Shrestha
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
